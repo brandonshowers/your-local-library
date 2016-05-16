8b85ea9220807eaaa4f9d6e25be30db55cb62ed8
Brandon Showers <brandon.showers@gmail.com>
2016-05-16 10:17:19 -0700

    Cleanup by Removing Whitespace and Removing Backbone

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 class YourLocalLibrary < Sinatra::Base
 	# Logical paths to your assets
-  set :assets_prefix, %w(assets vendor/assets node_modules)
+	set :assets_prefix, %w(assets vendor/assets node_modules)
 
 	register Sinatra::AssetPipeline
 
--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,4 +1,2 @@
 /* See https://github.com/rails/sprockets#behavior to learn how to import files
 into the application */
-
-//= require node_modules/backbone/backbone-min.js
